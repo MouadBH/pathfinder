b9d3e00cdccc1a4c1324604d4be5c34e50d0b0ca
Mouad Boulahdoud <mouad123b@gmail.com>
2021-03-02 01:24:11 +0100

    Add Node Component

---
+++ src/ui/node/Node.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+
+export default class Node extends Component {
+  render() {
+    const {
+      col,
+      isEnd,
+      isStart,
+      isWall,
+      onMouseDown,
+      onMouseEnter,
+      onMouseUp,
+      row,
+    } = this.props;
+    const extraClassName = isEnd
+      ? 'node-finish'
+      : isStart
+      ? 'node-start'
+      : isWall
+      ? 'node-wall'
+      : '';
+
+    return (
+      <div
+        id={`node-${row}-${col}`}
+        className={`node ${extraClassName}`}
+        onMouseDown={() => onMouseDown(row, col)}
+        onMouseEnter={() => onMouseEnter(row, col)}
+        onMouseUp={() => onMouseUp()}></div>
+    );
+  }
+}
